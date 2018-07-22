8014255b5269664df4ac07040845796c647195f3
gaaamii <i1shi4ga4mi1@gmail.com>
2018-07-22 23:19:32 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # set symlinks on Home directory
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -sf $HOME/dotfiles/.vim/colors $HOME/.vim/colors
+ln -sf $HOME/dotfiles/.vim/colors/molokai.vim $HOME/.vim/colors/molokai.vim
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/dotfiles/Brewfile $HOME/Brewfile
 
@@ -16,3 +16,21 @@ fi
 brew tap Homebrew/bundle
 brew bundle
 brew bundle cleanup
+
+# install anyenv
+# https://github.com/riywo/anyenv
+if ! [ `which anyenv` ]; then
+  git clone https://github.com/riywo/anyenv ~/.anyenv
+  echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(anyenv init -)"' >> ~/.bashrc
+
+  # shell relogin
+  exec $SHELL -l
+fi
+
+# install *env
+anyenv install rbenv
+anyenv install ndenv
+
+# shell relogin
+exec $SHELL -l
