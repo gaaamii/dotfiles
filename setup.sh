11da566ea0ad3096454ff71523817081664de433
gaaamii <i1shi4ga4mi1@gmail.com>
2018-07-22 22:48:06 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #/bin/bash
 
+# set symlinks on Home directory
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/dotfiles/.vim/colors $HOME/.vim/colors
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/Brewfile $HOME/Brewfile
+
+# install homebrew
+if ! [ `which brew` ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# brew install
+brew tap Homebrew/bundle
+brew bundle
+brew bundle cleanup
